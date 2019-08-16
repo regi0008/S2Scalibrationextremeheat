1d9c4b2113edd1be72bce9cb8a821df66c15cc98
regi0008 <53603551+regi0008@users.noreply.github.com>
2019-08-16 15:04:42 +0800

    Still has errors to be solved (.ncml problem)

--- loadeR/aggregateDataset.R
+++ loadeR/aggregateDataset.R
@@ -1,4 +1,8 @@
 #aggregate hindcast dataset into one
+#makeAggregatedDataset(source.dir, ncml.file, file.ext = "nc",
+#                      aggr.dim = "time", pattern = NULL, recursive = FALSE,
+#                      verbose = TRUE, timeUnitsChange = TRUE)
+
 
 #load necessary libraries
 library(loadeR)
@@ -6,15 +10,18 @@ library(downscaleR)
 library(transformeR)
 library(ncdf4)
 
-dir <- file.path("C:/Users/regin/Desktop/R/Datasets_for_project/ecmwf_hindcast_data_c3s_regine/temp/201902")
+dir <- file.path("C:/Users/regin/Desktop/R/Datasets_for_project/hindcast_dataset")
 list.files(dir, pattern = "\\.nc$")
 
-#current problem below
+#makeAggregatedDataset() returns a NcML file at a specified location
+#from multiple files aggregated together along a selected dimension.
+#!!current problem below returned!!:
+#error in file(ncml.file, "w"): cannot open the connection, cannot open ncml file.
 makeAggregatedDataset(source.dir = dir,
-                      ncml.file = "C:/Users/regin/Desktop/R/temp/ncml_test.ncml",
+                      ncml.file = "C:/Users/regin/Desktop/R/hindcast_dataset/hindcast.ncml",
+                      aggr.dim = "2 metre temperature",
                       verbose = TRUE)
 
-di <- dataInventory(dataset = "C:/Users/regin/Desktop/R/temp/ncml_test.ncml")
+di <- dataInventory(dataset = "C:/Users/regin/Desktop/R/hindcast_dataset/ncml_test.ncml")
 
 #names(di)
-#str(di$Q)
