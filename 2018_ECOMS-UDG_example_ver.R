b557057f841afd2e4e3e9a619a58774c6ff2c9c9
regi0008 <53603551+regi0008@users.noreply.github.com>
2019-08-22 15:01:48 +0800

    Add files via upload

--- 2018_ECOMS-UDG_example_ver.R
+++ 2018_ECOMS-UDG_example_ver.R
@@ -84,9 +84,8 @@ spatialPlot(climatology(tas.cfs),
 
 #-----------------------------------------------------------
 
-#Correlation Analysis
+#Ensemble Correlation
 #object cor contains correlation coefficients for each point.
-#need to convert list into array
 
 obs <- tas.ncep[["Data"]]                 #type = double
 #obs_array <- array(as.numeric(unlist(obs)))
@@ -105,7 +104,7 @@ cor <- veriApply(verifun = "EnsCorr",
                  ensdim = 1)
 
 #easyVeri2grid is a bridging function that performs transformation using transformeR
-#from the raw verification matrices produced by library(easyVerification)
+#transforms the raw verification matrices produced by library(easyVerification)
 #to the grid structure with all required metadata for data collection and representation by climate4R bundle
 cor.grid <- easyVeri2grid(easyVeri.mat = cor,
                           obs.grid = tas.ncep,
@@ -122,3 +121,58 @@ spatialPlot(climatology(cor.grid),
             sub = "CFSv2 24 member - DJF Mean Temperature (1982-2010")
 
 #-----------------------------------------
+
+#ROC Area
+#recall - observation: tas.ncep, forecast: tas.cfs
+
+#EnsRoca computes the area under the ROC curve given the observations.
+
+roca <- veriApply(verifun = "EnsRoca",
+                  fcst = fcst,
+                  obs = obs,
+                  prob = c(1/3,2/3),    #Tercile Probabilities
+                  tdim = 2,
+                  ensdim = 1)
+
+l <- lapply(roca[1:3], "easyVeri2grid", tas.ncep)
+mg <- makeMultiGrid(l)
+str(mg)
+
+#-----------------------------------------
+
+#plot climatology
+
+spatialPlot(mg,
+            backdrop.theme = "countries",
+            color.theme = "RdYlBu",
+            names.attr = c("Lower tercile", "Middle tericle", "Upper tericle"),
+            layout =c(3,1),
+            main = "Area under the ROC curve",
+            sub = "CFSv2 24 member - DJF Mean Temperature (1983-2010)")
+
+#-----------------------------------------
+
+#Compute Forecast/Observed Anomalies
+
+library(s2dverification)
+
+#still have problems from here onwards
+tas.cfs.array <- array(as.numeric(as.character(unlist(tas.cfs))))
+tas.ncep.array <- array(as.numeric(as.character(unlist(tas.ncep))))
+
+ano_tas.cfs <- Ano(tas.cfs.array, climatology(tas.cfs))
+ano_tas.ncep <- Ano(tas.ncep.array, climatology(tas.ncep))
+#runmean_nb_months <- 12
+#dim_to_smooth <- 4 #smooth along lead-times
+#smooth_ano_tascfs <- smoothing(ano_tascfs, runmean_nb_months, dim_to_smooth)
+#smooth_ano_tasncep <- smoothing(ano_tasncep, runmean_nb_months, dim_to_smooth)
+
+#Plot time series of smoothed anomalies
+#error!!
+PlotAno(ano_tas.cfs, ano_tas.ncep,
+        toptitle = paste('smoothed anomalies'),
+        ytitle = c('K', 'K', 'K'),
+        fileout = 'anomalies')
+
+#-----------------------------------------
+
