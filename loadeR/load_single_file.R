b50b73d8b68661f5302a0d507d97b0e72f117f43
regi0008 <53603551+regi0008@users.noreply.github.com>
2019-08-23 15:31:29 +0800

    Create load_single_file.R

---
+++ loadeR/load_single_file.R
@@ -0,0 +1,18 @@
+#to load a single local file
+
+library(loadeR)
+library(visualizeR)
+# change the dir link only
+dir <- "C:/Users/regin/Desktop/R/Datasets_for_project/ecmwf_hindcast_data_c3s_regine/temp/201902"
+list.files(dir, pattern = ".grib$")
+
+di <- dataInventory(file.path(dir, "2t_19930201.grib"))
+str(di)
+
+pr <- loadGridData(dataset = file.path(dir, "2t_19930201.grib"), var = "2_metre_temperature_surface")                  
+str(pr)
+
+#Plot the mean value of Feb, Mar, Apr and May 2019
+spatialPlot(climatology(pr, clim.fun = list(FUN = mean, na.rm = T)), 
+            backdrop.theme = "countries", 
+            scales = list(draw = T), rev.colors = TRUE)
