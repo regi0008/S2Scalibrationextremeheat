c65420774d3171dbbdf40ffa1f442fc80f735da8
regi0008 <53603551+regi0008@users.noreply.github.com>
2019-09-05 14:35:25 +0800

    Update calibratedhindcast_March_Verification_CRPS.R

--- Verification in R/CALIBRATED HINDCAST vs OBS (MARCH)/calibratedhindcast_March_Verification_CRPS.R
+++ Verification in R/CALIBRATED HINDCAST vs OBS (MARCH)/calibratedhindcast_March_Verification_CRPS.R
@@ -195,25 +195,6 @@ obs <- loadNcdf(file.path(dir2, "2t_era5_Mar_1993_2016_format.nc"), "tas")
 #Using library(easyVerification) and library(SpecsVerification)
 #we can use veriApply function to get Crps
 
-#https://github.com/cran/SpecsVerification/blob/master/src/enscrps.cpp
-EnsCrps = function(ens, obs, R.new=NA) {
-  
-  stopifnot(is.matrix(ens), 
-            nrow(ens) == length(obs), 
-            length(R.new)==1)
-  
-  crps = enscrps_cpp(ens, obs, R.new)
-  
-  # return the vector of crps
-  return(crps)
-  
-}
-
-FairCrps = function(ens, obs) {
-  return(EnsCrps(ens, obs, R.new=Inf))
-}
-
-
 crps_results_ens <- veriApply(verifun = "EnsCrps",
                  fcst = fcst_cal$Data,
                  obs = obs$Data,
