efb4ed63074ce6c5868ae39feac44d2d147f3f64
regi0008 <53603551+regi0008@users.noreply.github.com>
2019-09-10 23:40:38 +0800

    Create calibratedhindcast_March_Verification_ReliabilityCategories_ryan.R

---
+++ VerificationinR/CALIBRATEDHINDCASTvsOBS(MARCH)/calibratedhindcast_March_Verification_ReliabilityCategories_ryan.R
@@ -0,0 +1,121 @@
+library(loadeR)
+library(downscaleR)
+library(transformeR)
+library(visualizeR)
+library(calibratoR)
+library(ncdf4)
+library(abind)
+library(s2dverification)
+library(SpecsVerification)
+library(easyVerification)
+library(verification)
+library(RColorBrewer)
+library(sp)
+library(rgdal)
+
+loadNcdf <- function(filePath, varname, tz = 'GMT', ...) {
+  nc <- nc_open(filePath)
+  var <- nc$var
+  # Use name to locate the variable
+  call_1 <- as.call(c(
+    list(as.name('$'), var, varname)
+  ))
+  var <- eval(call_1)
+  if(is.null(var)) stop('No such variable name, check source file.')
+  
+  dimNames <- unlist(lapply(1:length(var$dim), function(x) var$dim[[x]]$name))
+  print(dimNames)
+  
+  # Only deals with the most common dimensions, futher dimensions will be added in future.
+  dimIndex <- grepAndMatch(c('lon', 'lat', 'time', 'member'), dimNames)
+  print(dimIndex)
+  if (length(dimIndex) < 3) stop('Your file has less than 3 dimensions.')
+  
+  # First needs to identify the variable name, load the right data
+  message('Loading data...')
+  nc_data <- ncvar_get(nc, var)
+  message('Processing...')
+  
+  gridData <- list()
+  gridData$Variable$varName <- varname
+  gridData$xyCoords$x <- var$dim[[dimIndex[1]]]$vals
+  attributes(gridData$xyCoords$x)$name <- dimNames[dimIndex[1]]
+  
+  gridData$xyCoords$y <- var$dim[[dimIndex[2]]]$vals
+  attributes(gridData$xyCoords$y)$name <- dimNames[dimIndex[2]]
+  
+  # Time part needs to be taken seperately
+  timeUnit <- strsplit(var$dim[[dimIndex[3]]]$units, split = ' since')[[1]][1]
+  timeDiff <- var$dim[[dimIndex[3]]]$vals
+  # To get real time, time since when has to be grabbed from the dataset.
+  timeSince <- as.POSIXlt(strsplit(var$dim[[dimIndex[3]]]$units, split = 'since')[[1]][2], tz = tz)
+  
+  #  Date <- rep(timeSince, length(timeDiff))
+  unitDic <- data.frame(weeks = 'weeks', days = 'days', hours = 'hours',
+                        minutes = 'mins', seconds = 'secs')
+  
+  timeDiff <- as.difftime(timeDiff, units = as.character(unitDic[1, timeUnit]))
+  
+  #   if (grepl('day', timeUnit)) {
+  #     Date$mday <- Date$mday + timeDiff
+  #   } else if (grepl('second', timeUnit)) {
+  #     Date$sec <- Date$sec + timeDiff
+  #   }
+  Date <- timeSince + timeDiff
+  
+  # data directly loaded from ncdf4 will drop the dimension with only one value.
+  # the varsize shows the real dimension, without any dropping.
+  dim(nc_data) <- var$varsize
+  
+  # Right now there is no need to add end Date, in furture, may be added as needed.
+  gridData$Dates$start <- as.character(Date)
+  
+  # Adding data to grid data
+  # At leaset should be 3 dimensions, lon, lat, time.
+  gridData$Data <- nc_data
+  
+  attributes(gridData$Data)$dimensions <- dimNames
+  
+  if (!is.na(dimIndex[4])) 
+    gridData$Members <- var$dim[[dimIndex[4]]]$vals
+  nc_close(nc)
+  
+  output <- gridData
+  
+  return(output)
+  
+}
+
+# in order to first grep than match.
+# match only provides for exactly match, 
+# dimIndex <- grepAndMatch(c('lon', 'lat', 'time', 'member'), dimNames)
+grepAndMatch <- function(x, table) {
+  index <- unlist(lapply(x, function(x) {
+    a <- grep(x, table)
+  }))
+  return(index)
+}
+#------------------------------------------
+#LOADING OF FILES THROUGH HYFO PACKAGE:
+#need to make sure dimensions of both files are the same:
+#calibrated hindcast: member, time, lat, lon
+#obs_formatted: time, lat, lon
+#dir1 <- "C:/Users/regin/Desktop/R/S2Scalibrationextremeheat"
+dir1 <- "C:/Users/Work/Desktop/Regine_project/data"
+#predictor (calibrated hincast):
+fcst_cal <- loadNcdf(file.path(dir1, "fcst_cal_CCR_asc.nc"), "tas")
+
+#dir2 <- "C:/Users/regin/Desktop/R/S2Scalibrationextremeheat/loadeR"
+dir2 <- "C:/Users/Work/Desktop/Regine_project/data"
+#predictand (observation):
+obs <- loadNcdf(file.path(dir2, "2t_era5_Mar_1993_2016_format_asc.nc"), "tas")
+#------------------------------------------
+
+shape_file <-readOGR(dir1,"merged_10_regions")
+
+#COMPUTE Reliability Categories
+
+output <- reliabilityCategories(fcst_cal, obs, regions = shape_file, n.events = 3,
+                              labels = c("Lower", "Middle", "Upper"),
+                              diagrams = TRUE, cex0 = 0.5, cex.scale = 20,
+                              layout = c(1,3),backdrop.theme = "countries")
